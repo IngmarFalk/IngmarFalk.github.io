99a37250278c8a17b1505441c4c5f4f526c1afb0
Ingmar Falk <if222gq@student.lnu.se>
2022-08-17 09:37:36 +0200

    V29

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "7e0d3d74efb7953940825aa6c1621acb",
-"index.html": "e9f833413c5cfa2556156a08a80b8b05",
-"/": "e9f833413c5cfa2556156a08a80b8b05",
-"main.dart.js": "1deeab2a0abb216f50673d62a1fecaf9",
+"index.html": "57a244a5bf8518afd29b16b62c246eda",
+"/": "57a244a5bf8518afd29b16b62c246eda",
+"main.dart.js": "6d95c7af338064c2e12e1bf98df808c7",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
